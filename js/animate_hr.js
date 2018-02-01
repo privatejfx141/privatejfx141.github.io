9814e4298f9d94a9f61da2f0a8ca0007b321975a
Jeffrey L <jeffclub51@yahoo.ca>
2018-01-31 23:33:14 -0500

    Rewrote site with Bootstrap
    Rewrote site with Bootstrap, front page in the works.

---
+++ js/animate_hr.js
@@ -0,0 +1,17 @@
+function growLine() {
+  var elem = document.getElementById("animate_hr");
+  var percent = 0;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (percent == 70) {
+      clearInterval(id);
+    } else {
+      percent++;
+      elem.style.width = percent + '%';
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  growLine();
+}, false);
