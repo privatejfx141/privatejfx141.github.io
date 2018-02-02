42fb254175c6d2992b52966facd121765e851039
Jeffrey L <jeffclub51@yahoo.ca>
2018-02-01 21:19:35 -0500

    Expanded cover, added links
    Expanded cover width, added social media links in footer.

--- js/animate_hr.js
+++ js/animate_hr.js
@@ -1,7 +1,7 @@
 function growLine() {
   var elem = document.getElementById("animate_hr");
   var percent = 0;
-  var id = setInterval(frame, 10);
+  var id = setInterval(frame, 15);
   function frame() {
     if (percent == 70) {
       clearInterval(id);
